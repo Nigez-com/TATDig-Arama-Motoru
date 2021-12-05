bd5029d245dd3c612c9983fa15fa1825f8508475
TATDIG <32492514+tatdig@users.noreply.github.com>
2021-12-05 13:01:34 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="Turkic Search Engine" />
+<meta property="og:description" content="Powered by TATDig API" />
+<title>Arama Motoru</title>
+</head>
+<body>
+</body>
+</html>
