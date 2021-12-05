425a566d475c74059bc0c5d715386da53df50a75
TATDIG <32492514+tatdig@users.noreply.github.com>
2021-12-05 13:08:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!doctype html>
 <html lang="en" dir="ltr">
-
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta property="og:type" content="website" />
 <meta property="og:site_name" content="Turkic Search Engine" />
 <meta property="og:description" content="Powered by TATDig API" />
+<link rel="stylesheet" type="text/css" href="/css/style.css">
 <title>Arama Motoru</title>
+<script src="/js/functions.js">
 </head>
 <body>
+  
 </body>
 </html>
