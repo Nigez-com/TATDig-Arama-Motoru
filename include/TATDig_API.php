cf7d0111376e804d93b71a9273e7d00e7a199596
Татар челтәр ятьмәсендә эзләү <nigez@tdcoincore.org>
2021-12-06 19:31:13 -0500

    Second commit working website

--- include/TATDig_API.php
+++ include/TATDig_API.php
@@ -1,5 +1,43 @@
 <?php
 /*
-TATDig API 2021
-
+*	TATDIG API use example with php and curl
+*	copyright TATDIG
 */
+
+define('API_KEY','NZykTlrU3tcC1pn5'); //change this to your API key
+define('HOSTNAME','arama.nigez.com'); //change this for your domain
+
+function get_tatdig_results($apikey,$num,$page,$q,$host){
+
+//sunucu adresi
+$url = 'https://www.nigez.com/api_service.php';
+// sunucuya aktarım için alanların hazırlanması
+$fields = array(
+            'apikey'=>urlencode($apikey),
+            'num'=>urlencode($num),
+            'page'=>urlencode($page),
+            'q'=>urlencode($q),
+            'host'=>urlencode(HOSTNAME)
+);
+
+//verileri uygun bir formata dönüştürmek
+$fields_string = '';
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+$fields_string = rtrim($fields_string,'&');
+
+//açık bağlantı
+$ch = curl_init();
+
+//özelleştirme curl
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_POST,count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//istek gönder sonuç al
+$result = curl_exec($ch);
+
+//hata ayıklama yazdırma
+
+return $result;
+}
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en" dir="ltr">
+<html lang="tr" dir="ltr">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta property="og:type" content="website" />
@@ -7,9 +7,36 @@
 <meta property="og:description" content="Powered by TATDig API" />
 <link rel="stylesheet" type="text/css" href="/css/style.css">
 <title>Arama Motoru</title>
-<script src="/js/functions.js">
+<script src="/js/functions.js"></script>
 </head>
 <body>
-  
+<div id="content" class="vertical-center">
+    <center>
+        <form>
+            <input id="search" name="search" type="search" value="<?php echo $_GET['search']; ?>">
+            <input type="submit" id="go" name="go" value="Go!">
+        </form>
+<?php
+    if( !array_key_exists('search',$_GET) || empty(trim($_GET['search'])) ){ ?>
+
+<?php }else{
+    require_once('include/TATDig_API.php');
+    $results = get_tatdig_results(API_KEY,5,$page,trim($_GET['search']),HOSTNAME);
+
+    $xml = new DOMDocument( "1.0", "utf-8" );
+    $xml->loadXML($results);
+    foreach ($xml->getElementsByTagName('result') as $result) {
+	echo "<div class=\"result\">";
+	echo "<a href=\"".$result->getAttribute('url')."\">".$result->getAttribute('title')."</a><br>";
+	echo "<p>".$result->getElementsByTagName('descr')[0]->textContent."</p>";
+	echo $result->getAttribute('title')."<br>";
+	echo "</div> <!-- result --> <hr>"; 
+    }
+ } ?>
+
+        <h1>TATDig arama motoru</h1>
+    <p>Paging controller could be here</p>
+    </center>
+</div>
 </body>
 </html>
